ed5d604857f9ea8db848ac9f91721631b61ecc93
emanegux <dev.evanpiro@gmail.com>
2021-04-22 13:30:37 -0400

    added onboarding deps

---
+++ services/onboard/src/lambda-handler.js
@@ -0,0 +1,104 @@
+const Joi = require("joi");
+const { Stripe } = require("stripe");
+const config = require("./config.js");
+const { configKey } = require("./enums");
+
+const stripeUserId = "acct_1IPBGPRDfW8UMKAc";
+
+require('dotenv').config()
+
+const runenv = process.env.RUNENV
+
+
+const onboard  = async (
+  stripeAccount,
+  amount,
+  cardNumber,
+  cardExpirationMonth,
+  cardExpirationYear,
+  cardCVC
+) => {
+  const stripeApiKey = await config.get(runenv, configKey.stripeApiKey);
+
+  const stripe = new Stripe(stripeApiKey, {
+    apiVersion: "2020-08-27",
+  });
+
+  const methodRes = await stripe.paymentMethods.create({
+    type: "card",
+    card: {
+      number: cardNumber,
+      exp_month: cardExpirationMonth,
+      exp_year: cardExpirationYear,
+      cvc: cardCVC,
+    },
+  });
+
+  const paymentMethodId = methodRes.id;
+
+  const res = await stripe.paymentIntents.create({
+    amount,
+    currency: "usd",
+    payment_method: paymentMethodId,
+    confirm: true,
+    transfer_data: {
+      destination: stripeAccount,
+    },
+  });
+
+  const { id: stripeTxnId } = res;
+
+  return stripeTxnId;
+};
+
+const contribSchema = Joi.object({
+  cardNumber: Joi.string().required(),
+  cardExpirationMonth: Joi.number().required(),
+  cardExpirationYear: Joi.number().required(),
+  cardCVC: Joi.string().required(),
+  amount: Joi.number().required(),
+});
+
+let response;
+
+module.exports = async (event, context) => {
+  const res = contribSchema.validate(JSON.parse(event.body));
+  if (res.error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: res.error.message,
+      }),
+    };
+  }
+
+  const {
+    amount,
+    cardNumber,
+    cardExpirationMonth,
+    cardExpirationYear,
+    cardCVC,
+  } = res.value;
+
+  try {
+    await executePayment(
+      stripeUserId,
+      amount,
+      cardNumber,
+      cardExpirationMonth,
+      cardExpirationYear,
+      cardCVC
+    );
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "success",
+      }),
+    };
+  } catch (err) {
+    return err;
+  }
+
+  return response;
+};
