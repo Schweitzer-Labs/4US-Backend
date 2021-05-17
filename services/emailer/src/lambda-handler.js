ede683f658c39812a052a281472b8c33b2dbe431
Seemant Kulleen <seemant@schweitzerlabs.com>
2021-05-17 00:11:00 -0400

    restore email functionality.  sns queue notification to 4us admins is not yet re-enabled

--- services/emailer/src/lambda-handler.js
+++ services/emailer/src/lambda-handler.js
@@ -1,39 +1,53 @@
 const AWS = require("aws-sdk");
 AWS.config.update({ region: process.env.REGION });
-const ses = new AWS.SES();
+const ses = new AWS.SES()
+  , sns = new AWS.SNS()
+;
 const from_address = 'notification@policapital.net';
 
 /*
  * Helper Functions
  */
-const informAdmins = async (message) => {
+const notifyAdmins = async (message) => {
     const params = {
         Message: JSON.stringify(message)
       , TopicArn: process.env.SNS_TOPIC
     };
-    // const resp = await sns.publish(params).promise();
+    const resp = await sns.publish(params).promise();
     console.log("send SNS", resp);
-}; // informAdmins()
+}; // notifyAdmins()
 
-const sendEmail = async (addresses, message, template) => {
+const sendEmail = async (message, parameters) => {
     const params = {
-        Source      : from_address
-      , Template    : template
-      , Destination : {
-          ToAddresses: addresses
-        }
-      , TemplateData: JSON.stringify(message)
+        ...parameters
+      , Source              : from_address
+      , TemplateData        : JSON.stringify(message)
       , ConfigurationSetName: 'SNSDebugging'
     };
+    console.log("params", params);
     const resp = await ses.sendTemplatedEmail(params).promise();
     console.log("sent SES", resp);
     return resp;
 }; // sendEmail()
 
 const emailDonor = async (message) => {
+    const params = {
+        Template    : process.env.POS_RECEIPT
+      , Destination : {
+          ToAddresses: [message.email]
+        }
+    };
+    await sendEmail(message, params);
 }; // emailDonor()
 
-const emailCommittee = async (message) => {
+const emailCommittee = async (message, attrs) => {
+    const params = {
+        Template   : process.env.POS_RECORD
+      , Destination: {
+          ToAddresses: attrs.committee.stringValue.split(',')
+        }
+    };
+    await sendEmail(message, params);
 }; // emailCommittee()
 
 /*
@@ -41,11 +55,13 @@ const emailCommittee = async (message) => {
  */
 module.exports = async (event, context) => {
     for (const record of event.Records) {
-        let data = record.body;
-        console.log(record, record.messageAttributes, data);
+        let data  = JSON.parse(record.body)
+          , attrs = record.messageAttributes
+        ;
+        console.log(data);
 
-        emailDonor(data);
-        emailCommittee(data);
+        await emailDonor(data);
+        await emailCommittee(data, attrs);
     };
 
     return;
