ff3c7affadaa37133cb3e32b7527726277a826b8
emanegux <dev.evanpiro@gmail.com>
2021-05-16 20:21:54 -0400

    moved around files

--- services/graphql-api/src/either-tasks/get-unverified-transactions.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "elasticsearch";
-import { left, TaskEither } from "fp-ts/TaskEither";
-import { ApplicationError } from "../utils/application-error";
-
-export const getUnverifiedTransactions = (esClient: Client) => (
-  committee: any
-): TaskEither<ApplicationError, any> => {
-  return left(new ApplicationError("not yet implemented", {}));
-};
