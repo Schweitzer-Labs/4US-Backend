cb07ce939844207ee55bd16ba0b291f7155499e5
Seemant Kulleen <seemant@schweitzerlabs.com>
2021-05-18 19:40:38 -0400

    recorder -> receiver

--- services/policapital/receiver/src/lambda-handler.js
+++ services/policapital/receiver/src/lambda-handler.js
@@ -9,7 +9,6 @@ const getCommitteeDetails = async (committee) => {
     "angel-cruz": ["seemant@schweitzerlabs.com", "evan@schweitzerlabs.com"],
     "john-safford": ["awsadmin@schweitzerlabs.com"],
   };
-  console.log(emails[committee]);
   return { emails: emails[committee], timezone: "America/New_York" };
 }; // getCommitteeDetails()
 
@@ -31,9 +30,8 @@ const sendQueue = async (message) => {
       MessageGroupId: message.committee,
       QueueUrl: process.env.SQSQUEUE,
     };
-  console.log("sending to", params.QueueUrl);
   const resp = await sqs.sendMessage(params).promise();
-  console.log("sending SQS", resp);
+  console.log("sent SQS", resp);
 }; // sendQueue()
 
 /*
@@ -41,32 +39,23 @@ const sendQueue = async (message) => {
  */
 module.exports = async (event, context) => {
   for (const stream of event.Records) {
-    const record = stream.dynamodb,
-      row = record.NewImage,
-      date = new Date(record.ApproximateCreationDateTime * 1000);
-    console.log("record", record);
+    const record = stream.dynamodb
+      , data = AWS.DynamoDB.Converter.unmarshall(record.NewImage)
+      , timestamp = new Date(record.ApproximateCreationDateTime * 1000)
+    ;
+    console.log("data", data);
 
-    const data = {
-      id: record.id,
-      committee: row.committee.S,
-      timestamp: date,
-      donor: [row.firstName.S, row.lastName.S].join(" "),
-      timezone: "America/New_York",
-      email: row.email.S,
-      occupation: row.occupation.S,
-      employer: row.employer.S,
-      address1: row.addressLine1.S,
-      address2: row.addressLine2.S,
-      city: row.city.S,
-      state: row.state.S.toUpperCase(),
-      zip: row.postalCode.S,
-      phone: row.phoneNumber.S,
-      amount: (row.amount.N / 100).toFixed(2),
-      transaction: row.stripePaymentIntentId.S,
-      receipt: row.stripePaymentIntentId.S.slice(-8),
-      refcode: row.refCode.S || "N/A",
-      card: row.cardNumberLastFourDigits.S,
-    };
+    data.id = record.id;
+    data.timezone = 'America/New_York';
+    data.amount = (data.amount / 100).toFixed(2);
+    data.timestamp = timestamp;
+    data.state = data.state.toUpperCase();
+    data.receipt = data.stripePaymentIntentId.slice(-8);
+
+    data.occupation = data.occupation || '';
+    data.employer = data.employer || '';
+    data.refCode = data.refCode || 'N/A';
+
     await sendQueue(data);
   }
 };
