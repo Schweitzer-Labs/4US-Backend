2dd09de8d3c3a8b62802b41ff2d19525cb4c81b4
emanegux <dev.evanpiro@gmail.com>
2021-06-17 14:28:15 -0400

    configured webhook

--- services/src/stripe-webhook.lambda.ts
+++ services/src/stripe-webhook.lambda.ts
@@ -1,6 +1,8 @@
-import { APIGatewayEvent } from "aws-lambda";
+import { APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export default async (event: APIGatewayEvent) => {
+export default async (
+  event: APIGatewayEvent
+): Promise<APIGatewayProxyResult> => {
   const payload = JSON.parse(event.body);
 
   console.log(payload);
@@ -12,4 +14,9 @@ export default async (event: APIGatewayEvent) => {
     default:
       console.log(`Unhandled event type ${payload.type}`);
   }
+
+  return {
+    statusCode: 200,
+    body: "",
+  };
 };
