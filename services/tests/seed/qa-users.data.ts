dc82e3bd84b9c2c909b15ba82729643ac9055325
emanegux <dev.evanpiro@gmail.com>
2021-07-06 16:09:40 -0400

    added nelson to users

--- services/tests/seed/qa-users.data.ts
+++ services/tests/seed/qa-users.data.ts
@@ -2,4 +2,5 @@ export const qaUsers = [
   "29fb6f85-29c5-4558-9933-89c4bb9020cd",
   "754b0182-80da-4aff-a737-2bed024b14c4",
   "de681593-9a3a-42e5-b357-5b6774d26cca",
+  "424a72aa-8fb9-413d-af48-2a84f9bf1b40",
 ];
