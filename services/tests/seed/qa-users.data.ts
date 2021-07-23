34c4d634b891b63f250d00563d8eeccc10c2069a
emanegux <dev.evanpiro@gmail.com>
2021-07-23 11:19:09 -0400

    updated cognito id

--- services/tests/seed/qa-users.data.ts
+++ services/tests/seed/qa-users.data.ts
@@ -4,4 +4,5 @@ export const qaUsers = [
   "de681593-9a3a-42e5-b357-5b6774d26cca",
   "424a72aa-8fb9-413d-af48-2a84f9bf1b40",
   "0db7889c-7527-4df4-97e6-370e9ae78721",
+  "40decc05-06ca-46cb-bfc6-264ef51988c4",
 ];
