35aa5428fe2c2660132eca82a5aad06da704d885
Seemant Kulleen <seemant@schweitzerlabs.com>
2021-08-03 08:45:51 -0400

    Instead of allowing all origins, only allow the origin expressed in the given DNS_URL environment variable, which will usually be a subdomain of this function's api gateway's domain

--- services/src/utils/headers.ts
+++ services/src/utils/headers.ts
@@ -1,5 +1,7 @@
+const allow_origin = "https://" + process.env.DNS_URL
+
 export default {
   "Access-Control-Allow-Headers": "Content-Type",
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": allow_origin,
   "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
 };
