a6b8db40da449cb6a3fdb2d4676287663f5fa320
emanegux <dev.evanpiro@gmail.com>
2021-08-11 10:56:31 -0400

    factored out handler logic from refresh

--- services/src/transaction-event-dispatcher.lambda.ts
+++ services/src/transaction-event-dispatcher.lambda.ts
@@ -8,14 +8,17 @@ import {
 import { isLeft } from "fp-ts/Either";
 import { ApplicationError } from "./utils/application-error";
 import { Source } from "./utils/enums/source.enum";
-import { DynamoDB } from "aws-sdk";
+import { DynamoDB, DynamoDBStreams } from "aws-sdk";
 import { pipe } from "fp-ts/function";
 import { task, taskEither } from "fp-ts";
 import * as dotenv from "dotenv";
 import { sendContribSuccessMsgs } from "./pipes/send-contrib-success-msgs.pipe";
 import { validateDDBResponse } from "./repositories/ddb.utils";
 import { auditTxnStream, DdbEventName } from "./pipes/audit-txn-stream.pipe";
-import { initStratoConfig } from "./clients/dapp/dapp.decoders";
+import {
+  initStratoConfig,
+  IStratoSDKConfig,
+} from "./clients/dapp/dapp.decoders";
 import {
   getStratoENodeUrl,
   getStratoNodeUrl,
@@ -46,6 +49,7 @@ let eNodeUrl: string;
 let oauthClientId: string;
 let oauthClientSecret: string;
 let oauthOpenIdDiscoveryUrl: string;
+let res: any;
 
 export default async (event: DynamoDBStreamEvent): Promise<EffectMetadata> => {
   if (
@@ -75,39 +79,26 @@ export default async (event: DynamoDBStreamEvent): Promise<EffectMetadata> => {
   console.log(JSON.stringify(event));
   console.log("initiating ddb update loop");
 
+  const handlerWithConf =
+    handleStream(stratoConf)(auditLogsTableName)(committeesTableName)(txnTable)(
+      dynamoDB
+    );
+
+  const resCol: EffectMetadata[] = [];
   for (const stream of event.Records) {
-    const record = stream.dynamodb;
-    switch (stream.eventName) {
-      case DdbEventName.INSERT:
-        console.log("INSERT event emitted");
-        const insertedTxn = await streamRecordToTxn(record.NewImage);
-        return await handleInsert(sqsUrl)(aggsTable)(txnTable)(
-          committeesTableName
-        )(dynamoDB)(insertedTxn);
-      case DdbEventName.MODIFY:
-        console.log("modify call");
-        console.log("record: ", JSON.stringify(record));
-        return await pipe(
-          auditTxnStream(stratoConf)(committeesTableName)(txnTable)(
-            auditLogsTableName
-          )(dynamoDB)(DdbEventName.MODIFY)(record.OldImage)(record.NewImage),
-          taskEither.chain((auditLog) =>
-            pipe(
-              refreshAggs(aggsTable)(txnTable)(dynamoDB)(
-                auditLog.newTransaction.id
-              ),
-              taskEither.map(() => auditLog)
-            )
-          ),
-          taskEither.fold(
-            (err) => task.of(failedAuditPut(DdbEventName.MODIFY)(err)),
-            (log) => task.of(successfulAuditPut(DdbEventName.MODIFY)(log))
-          )
-        )();
-      default:
-        return;
-    }
+    resCol.push(await handlerWithConf(stream));
   }
+
+  const exampleTxn: any = event.Records[0].dynamodb.NewImage;
+
+  await pipe(
+    taskEither.of(AWS.DynamoDB.Converter.unmarshall(exampleTxn)),
+    taskEither.chain(validateDDBResponse("Txn")(Transaction)),
+    taskEither.map((txn) => txn.committeeId),
+    taskEither.chain(refreshAggs(aggsTable)(txnTable)(dynamoDB))
+  )();
+
+  return resCol[0];
 };
 
 const streamRecordToTxn = async (txnRecord: any): Promise<ITransaction> => {
@@ -181,8 +172,6 @@ const failedAuditPut =
 
 const handleInsert =
   (sqsUrl: string) =>
-  (aggsTable: string) =>
-  (txnTable: string) =>
   (committeesTableName: string) =>
   (dynamoDB: DynamoDB) =>
   async (txn: ITransaction): Promise<EffectMetadata> => {
@@ -199,8 +188,40 @@ const handleInsert =
         )
       )();
 
-      await refreshAggs(aggsTable)(txnTable)(dynamoDB)(txn.committeeId)();
-
       return res;
     }
+  };
+
+const handleStream =
+  (stratoConf: IStratoSDKConfig) =>
+  (aLogsTableName: string) =>
+  (comTable: string) =>
+  (txnsTable: string) =>
+  (ddb: DynamoDB) =>
+  async (stream: any): Promise<EffectMetadata> => {
+    const record = stream.dynamodb;
+    let res: any;
+    switch (stream.eventName) {
+      case DdbEventName.INSERT:
+        console.log("INSERT event emitted");
+        const insertedTxn = await streamRecordToTxn(record.NewImage);
+        res = await handleInsert(sqsUrl)(committeesTableName)(dynamoDB)(
+          insertedTxn
+        );
+        break;
+      case DdbEventName.MODIFY:
+        console.log("modify call");
+        console.log("record: ", JSON.stringify(record));
+        res = await pipe(
+          auditTxnStream(stratoConf)(comTable)(txnsTable)(aLogsTableName)(ddb)(
+            DdbEventName.MODIFY
+          )(record.OldImage)(record.NewImage),
+          taskEither.fold(
+            (err) => task.of(failedAuditPut(DdbEventName.MODIFY)(err)),
+            (log) => task.of(successfulAuditPut(DdbEventName.MODIFY)(log))
+          )
+        )();
+    }
+
+    return res;
   };
