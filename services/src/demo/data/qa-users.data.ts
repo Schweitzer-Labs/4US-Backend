e21706f658445ef1fbb8a97b8a8f1f3b5da06799
emanegux <dev.evanpiro@gmail.com>
2021-08-20 11:02:55 -0400

    added demo cognito ids

--- services/src/demo/data/qa-users.data.ts
+++ services/src/demo/data/qa-users.data.ts
@@ -5,4 +5,9 @@ export const qaUsers = [
   "424a72aa-8fb9-413d-af48-2a84f9bf1b40",
   "0db7889c-7527-4df4-97e6-370e9ae78721",
   "40decc05-06ca-46cb-bfc6-264ef51988c4",
+  "0eec7b20-0cd5-407e-b64c-16e1f63c4e4d",
+  "cbe229cc-a70e-4bce-adf5-092e57694393",
+  "cee2ccf2-bf66-4a0d-82db-f31ec8bcacec",
+  "d973fffb-f463-40c8-8a2d-0a2a6fdafd72",
+  "fc87bb25-80e9-456c-a537-0350e112501f",
 ];
