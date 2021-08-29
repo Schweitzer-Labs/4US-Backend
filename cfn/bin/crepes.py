f8792ca86ebaf28de4ceaf8154353d4a354f9568
Seemant Kulleen <seemant@schweitzerlabs.com>
2021-08-29 18:57:22 -0400

    update crepes to the latest, which takes in keyword arguments

--- cfn/bin/crepes.py
+++ cfn/bin/crepes.py
@@ -8,58 +8,26 @@ from cfn_tools import load_yaml, dump_yaml, dump_json
 from jinja2 import Template
 
 #
-# Available command line arguments and their defaults
-#
-parser = argparse.ArgumentParser(description='process jinja cloudformation yaml files and assemble into a CloudFormation template')
-parser.add_argument('directory', metavar='dir', type=str, help='src directory')
-parser.add_argument('--region', dest='region', type=str, default='us-west-1', help='AWS Region')
-parser.add_argument('--output', dest='outfile', type=str, help='output CloudFormation YAML file')
-parser.add_argument('--import', dest='imports', type=str, help='import a subset of existing resources into the stack')
-parser.add_argument('--runenv', dest='runenv', type=str, help='lambda run environment (prod/qa/demo)')
-parser.add_argument('--domain', dest='domain', type=str, help="domain name (e.g. 'example' from 'www.example.com')")
-parser.add_argument('--subdomain', dest='subdomain', type=str, help="subdomain (e.g. 'www' from 'www.example.com')")
-parser.add_argument('--tld', dest='tld', type=str, help="tld (e.g. 'com' from 'www.example.com')")
-parser.add_argument('--product', dest='product', type=str, help="the product (p2 or 4us) for which we are building this template")
-parser.add_argument('--hostid', dest='hostid', type=str, help="route53 hostid")
-
-
-#
 # Set Global Variables
 #
-# Parse command line arguments and process the information
-args = parser.parse_args()
-src = os.path.dirname(args.directory)
-stack = os.path.basename(args.directory)
-
-runenv = args.runenv or 'dev'
-subdomain = args.subdomain or ''
-domain = args.domain or ''
-tld = args.tld or ''
-
-# Find AWS metadata for this stack deployment
-ec2 = boto3.setup_default_session(region_name=args.region)
-ec2 = boto3.client('ec2')
-zones = ec2.describe_availability_zones()['AvailabilityZones']
-AZs = [z['ZoneName'] for z in zones]
-AZcodes = [z.split('-')[2] for z in AZs]
-
-
 imports = {}
+
 
 #
 # Helper functions
 #
+
 # Read raw yaml file and process as a Jinja template
-#
-def process_jinja_template(filename):
+def process_jinja_template(filename, kwargs):
     with open(filename) as f:
         contents = f.read()
 
     template = Template(contents)
-    return template.render(AZs=AZs, AZcodes=AZcodes, REGION=args.region, RUNENV=runenv, SUBDOMAIN=subdomain, DOMAIN=domain, TLD=tld, PRODUCT=args.product, HOSTID=args.hostid)
+
+    return load_yaml(template.render(**kwargs))
 
 # Assemble all the components of a stack into a single cloudformation::stack object
-def assemble(stack):
+def assemble(stack, kwargs, importing=False):
     # Create a dictionary to hold all the information
     stackobj = { 'AWSTemplateFormatVersion': '2010-09-09' }
 
@@ -79,10 +47,10 @@ def assemble(stack):
 
                 print("Flipping %s" % os.path.relpath(os.path.join(dirpath, f), '.'))
                 # process each file as a jinja template first
-                contents = load_yaml(process_jinja_template(os.path.join(dirpath, f)))
+                contents = process_jinja_template(os.path.join(dirpath, f), kwargs)
 
                 try: # after jinja, we have pure yaml
-                    if sec == 'Resources' and args.imports:
+                    if sec == 'Resources' and importing:
                         keys = [key for key in contents.keys() if key in imports.keys()]
                     else:
                         keys = contents.keys()
@@ -102,12 +70,12 @@ def importify(resource, obj):
     obj['DeletionPolicy'] = 'Retain'
 
     temp = {}
-    for prop in imports[resource]['Property']:
-        print('Importing %s for %s' % (prop, resource))
-        try:
+    try:
+        for prop in imports[resource]['Property']:
+            print('Importing %s for %s' % (prop, resource))
             temp[prop] = obj['Properties'][prop]
-        except KeyError: # e.g. SNSTopic requires TopicARN for import, but it is not a property in the template
-            pass
+    except KeyError: # e.g. SNSTopic requires TopicARN for import, but it is not a property in the template
+        pass
 
     # Replace the Properties key with only the filtered items
     obj['Properties'] = temp
@@ -115,24 +83,56 @@ def importify(resource, obj):
     return { 'ResourceType': obj['Type'], 'LogicalResourceId': resource, 'ResourceIdentifier': imports[resource]['Matcher'] }
 
 
+def parse_command_line_arguments():
+    class ParseKwargs(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            setattr(namespace, self.dest, dict())
+            for value in values:
+                key, value = value.split('=')
+                getattr(namespace, self.dest)[key.upper()] = value
+
+
+    # Available command line arguments and their defaults
+    parser = argparse.ArgumentParser(description='process jinja yaml files and assemble into a CloudFormation template')
+    parser.add_argument('directory', metavar='dir', type=str, help='source directory')
+    parser.add_argument('--region', dest='region', type=str, help='AWS Region')
+    parser.add_argument('--output', dest='outfile', type=str, help='output CloudFormation YAML file')
+    parser.add_argument('--import', dest='imports', type=str, help='import existing resources into the stack')
+    parser.add_argument('--kwargs', dest='kwargs', nargs='*', action=ParseKwargs, help="list of KEY=value pairs")
+
+    # return the parsed command line arguments
+    return parser.parse_args()
+
+#
 # Main loop
+#
 def main():
     global imports
 
+    args = parse_command_line_arguments()
+
+    # Find AWS metadata for this stack deployment
+    ec2   = boto3.setup_default_session(region_name=args.region)
+    ec2   = boto3.client('ec2')
+    zones = ec2.describe_availability_zones()['AvailabilityZones']
+
+    args.kwargs['REGION']  = args.region
+    args.kwargs['AZs']     = [z['ZoneName'] for z in zones]
+    args.kwargs['AZcodes'] = [z.split('-')[2] for z in args.kwargs['AZs']]
+
     if args.imports: # process the ImportedResources file
-        imports = load_yaml(process_jinja_template('ImportedResources.yml'))
+        imports = process_jinja_template('ImportedResources.yml', args.kwargs)
 
     # Assemble the stack into a dict and convert that to yaml
-    stack = assemble(args.directory)
-
+    stack = assemble(args.directory, args.kwargs, importing=args.imports)
 
     if args.imports: # trim the stack based on the ImportedResources
         stack.pop('Outputs', None)
         imports_list = [importify(res, stack['Resources'][res]) for res in stack['Resources']]
 
         print(dump_json(imports_list))
-        with open(args.imports, 'w') as f: f.write(dump_json(imports_list))
-
+        with open(args.imports, 'w') as f:
+            f.write(dump_json(imports_list))
 
     formation = dump_yaml(stack)
 
