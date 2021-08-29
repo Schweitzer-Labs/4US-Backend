2959d398204c0381833da752a1c8e1ca690c7e63
Seemant Kulleen <seemant@schweitzerlabs.com>
2021-08-28 23:34:09 -0400

    add url redirector CloudFlare Worker code, to redirect committeeId query strings to the new url layout

---
+++ committee-redirector/index.js
@@ -0,0 +1,34 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
+
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  const redirectCode = 301;
+
+  const url = new URL(request.url)
+    , { searchParams } = url
+    , committee = searchParams.get('committeeId');
+  ;
+
+  if(searchParams.has('committeeId')) {
+    searchParams.delete('committeeId');
+  }
+
+  /* Rewrite the pathname and query string */
+  url.pathname = `/committee/${committee}`;
+  url.search = searchParams.toString();
+
+  return new Response(
+        ''
+      , { status: redirectCode, headers: { 'Location': url.href } }
+    )
+  ;
+}
