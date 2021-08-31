cb831a80207929a8d2b6d03eae7212ba491db011
emanegux <dev.evanpiro@gmail.com>
2021-08-31 11:21:45 -0400

    added more message metadata

--- services/src/pipes/txn-to-sqs.pipe.ts
+++ services/src/pipes/txn-to-sqs.pipe.ts
@@ -4,6 +4,7 @@ import { pipe } from "fp-ts/function";
 import { SendMessageRequest } from "aws-sdk/clients/sqs";
 import { taskEither } from "fp-ts";
 import { sendMessage } from "../utils/send-sqs.utils";
+import { now } from "../utils/time.utils";
 
 export const txnToSQS = (sqsUrl: string) => (sqs: SQS) => (txn: ITransaction) =>
   pipe(taskEither.of(toMsg(sqsUrl)(txn)), taskEither.chain(sendMessage(sqs)));
@@ -13,4 +14,6 @@ const toMsg =
   (txn: ITransaction): SendMessageRequest => ({
     MessageBody: JSON.stringify(txn),
     QueueUrl: sqsUrl,
+    MessageGroupId: txn.committeeId,
+    MessageDeduplicationId: `${now()}-${txn.id}`,
   });
