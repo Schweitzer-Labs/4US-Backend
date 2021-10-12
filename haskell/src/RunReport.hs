cfc72938d07e341316f020a59d7903387cc6e0cd
emanegux <dev.evanpiro@gmail.com>
2021-10-12 00:12:52 -0400

    form value encoding supported

--- haskell/src/RunReport.hs
+++ haskell/src/RunReport.hs
@@ -22,7 +22,18 @@ runRep = do
     apiKey <- getStripeAPIKey
     let opts = defaults & header "content-type" .~ ["application/x-www-form-urlencoded"] & auth ?~ oauth2Bearer (encodeUtf8 apiKey)
     res <- postWith opts endpoint
-        $ pairs
-            ( "report_type" .= ("balance_change_from_activity.itemized.3" :: String))
+        $ ( "report_type" := ("balance_change_from_activity.itemized.3" :: String) )
     return $ show res
+
+
+
+
+-- $ [ "report_type" := ("balance_change_from_activity.itemized.3" :: String)
+--  , "parameters[interval_start]" := (1577865600 :: Int)
+--  , "parameters[interval_end]" := (1580544000 :: Int)
+--  , "parameters[timezone]" := ("America/Los_Angeles" :: String)
+--  , "parameters[columns][]" := ("created" :: String)
+--  , "parameters[columns][]" := ("reporting_category" :: String)
+--  , "parameters[columns][]" := ("net" :: String)
+--  ]
 
