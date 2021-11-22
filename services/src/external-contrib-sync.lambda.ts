ba9aee48ff857e206916d5fb9591174511cb5325
emanegux <dev.evanpiro@gmail.com>
2021-11-22 15:49:45 -0500

    updated sqs url env ref

--- services/src/external-contrib-sync.lambda.ts
+++ services/src/external-contrib-sync.lambda.ts
@@ -15,12 +15,12 @@ AWS.config.apiVersions = {
 const ddb = new DynamoDB();
 const sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
 const comsTable: any = process.env.COMMITTEES_DDB_TABLE_NAME;
-const bankSQSUrl: any = process.env.ACTBLUE_SQS_URL;
+const sqsUrl: any = process.env.EXTERNAL_CONTRIB_SQS_URL;
 
 export default async () => {
-  const res = await actBlueToSqs(ActBlueCSVType.PaidContributions)(bankSQSUrl)(
-    sqs
-  )(comsTable)(ddb)();
+  const res = await actBlueToSqs(ActBlueCSVType.PaidContributions)(sqsUrl)(sqs)(
+    comsTable
+  )(ddb)();
   if (isLeft(res)) throw res.left;
 
   return "success";
